5b29c0c2614c3664e5171c2304780afdb637c84e
Lucasmiillers22 <lucasmiiller00007@hotmail.com>
2015-10-03 17:25:31 -0300

    Adventure
    Game feito pra SbGames para Android

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Button : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+	public GameObject player;
+	public string direction;
+	public void OnPointerDown (PointerEventData eventData) 
+	{
+		if(direction.Equals("UP"))
+			player.GetComponent<FPSInputController>().SendMessage("Up");
+		else if(direction.Equals("DOWN"))
+			player.GetComponent<FPSInputController>().SendMessage("Down");
+		else if(direction.Equals("LEFT"))
+			player.GetComponent<MouseLook>().SendMessage("Left");
+		else if(direction.Equals("RIGHT"))
+			player.GetComponent<MouseLook>().SendMessage("Right");
+
+
+	}
+	public void OnPointerUp(PointerEventData eventData) 
+	{
+		if(direction.Equals("UP") ||direction.Equals("DOWN"))
+			player.GetComponent<FPSInputController>().SendMessage("Null");
+		if(direction.Equals("LEFT") || direction.Equals("RIGHT"))
+			player.GetComponent<MouseLook>().SendMessage("Null");
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public AudioClip back;
+	public AudioClip letgo;
+	private GameObject ally;
+	void Start()
+	{
+		ally = GameObject.Find ("Ally");
+	}
+	void OnControllerColliderHit(ControllerColliderHit col) 
+	{
+		if(col.gameObject.tag.Equals("Tree"))
+			print ("Cuidado! ai tem uma arvore!");
+	}
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag.Equals ("Way")) 
+		{
+			ally.GetComponent<Animator>().enabled = true;
+			ally.GetComponent<AudioSource>().clip = letgo;
+		}
+	}
+	void OnTriggerExit(Collider col)
+	{
+		if (col.gameObject.tag.Equals ("Way")) 
+		{
+			ally.GetComponent<Animator> ().enabled = false;
+			ally.GetComponent<AudioSource> ().enabled = true;
+			ally.GetComponent<AudioSource>().clip = back;
+			ally.GetComponent<AudioSource>().Play();
+		}
+	}
+
+	void Update()
+	{
+	}
+}
